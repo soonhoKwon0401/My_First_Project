c4df9149b2d958af1619beded0a73f0d7fcce483
Kwon <kwonsh04011@gmail.com>
2026-02-01 23:56:17 +0900

    html 정리, css 정리

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-'use strict';
+"use strict";
 
-const goToAddPage = document.querySelector('.app__main')
-const appContent = document.querySelector('.app__content')
+const goToAddPage = document.querySelector(".app__main");
+const appContent = document.querySelector(".app__content-card");
 
-goToAddPage.addEventListener('click', (event) => {
-        const linkEl = event.target.closest('[data-link]')
-        if(!linkEl) return
-        event.preventDefault() 
+goToAddPage.addEventListener("click", (event) => {
+  const linkEl = event.target.closest("[data-link]");
+  if (!linkEl) return;
+  event.preventDefault();
 
-        const url = linkEl.dataset.link
-        history.pushState({}, "", url)
-        render();
-    })
+  const url = linkEl.dataset.link;
+  history.pushState({}, "", url);
+  render();
+});
 
 const routes = {
-    '/': () => `              
-                 <div class="app__content-card">
-                    <div class="add__content__container">
-                        <div class="add__content-heading">     
-                            <h2 class="add__content-title">단어를 추가해보세요</h2>
-                        </div>
-                        <div class="add__content__form">
-                            <button type="button"class="add__content__button" data-link="/add">추가하기</button>
-                        </div>
-                    </div>
-                </div>`,
-    '/add': () => 
-                //  `
-                // <div class="app__content-card">
-                //     <div class="add__content__container">
-                        
-                //     </div>
-                // </div>
-                // `
+  "/": () => `<div class="add__content__container">
+                  <h2 class="add__content-title">단어를 추가해보세요</h2>
+                  <button type="button"class="add__content__button" data-link="/add">추가하기</button>
+              </div>`,
+  "/add": () => `<div class="add__content__container">
+              <div class="add__content__header">
+                <div class="add__content__header-info">
+                  <span class="add__content_id"> 1 </span>
+                </div>
+                <div class="add__content__header-menu">
+                  <button class="add__content__delete-card">
+                  
+                  </button>
+                  <button class="add__content__menu-detail">
+
+                  </button>
+                </div>
+              </div>
+            </div>`,
+};
+
+function getCurrentPath() {
+  return window.location.pathname;
 }
 
-function getCurrentPath () {
-    return window.location.pathname
-    
+function notfoundView() {
+  return `<h1>Not Found</h1>`;
 }
 
-function notfoundView () {
-    return `<h1>Not Found</h1>`;
+function render() {
+  const path = getCurrentPath();
+  const view = routes[path] ?? notfoundView;
+  appContent.innerHTML = view();
 }
 
-function render () {
-    const path = getCurrentPath()
-    const view = routes[path] ?? notfoundView
-    appContent.innerHTML = view()
-}
-
-window.addEventListener('popstate', render)
+window.addEventListener("popstate", render);
 render();
