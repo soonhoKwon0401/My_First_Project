c3b0324b59188dde1f778059ac3637ce459499f0
Kwon <kwonsh04011@gmail.com>
2026-02-07 01:01:28 +0900

    localstoragy 수정

--- index.js
+++ index.js
@@ -4,8 +4,29 @@ const goToAddPage = document.querySelector(".app__main");
 const appContent = document.querySelector(".app__content-card");
 
 const wordForm = document.querySelector(".content__container__text-box");
-const sentenceInput = document.queSelector("#sentence");
+const sentenceInput = document.querySelector("#sentence");
 const meanInput = document.querySelector("#mean");
+
+wordForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  getInputValue();
+});
+
+function getInputValue() {
+  const sentence = sentenceInput.value.trim();
+  const mean = meanInput.value.trim();
+  if (!sentence || !mean) {
+    alert("문장 또는 뜻을 입력해주세요");
+  }
+  setWords(sentence, mean);
+}
+
+function setWords(sentence, mean) {
+  localStorage.setItem(sentence, mean);
+  console.log(localStorage.getItem(sentence));
+}
+// 뜻도 value 값을 이용해서 문장을 가져올 수 있는 방법은?
+// 저장 후 안내 메시지 추가
 
 goToAddPage.addEventListener("click", (event) => {
   const linkEl = event.target.closest("[data-link]");
@@ -15,7 +36,6 @@ goToAddPage.addEventListener("click", (event) => {
   const url = linkEl.dataset.link;
   history.pushState({}, "", url);
   render();
-  s;
 });
 
 // const routes = {
