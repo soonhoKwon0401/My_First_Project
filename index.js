f0b1f6760be57e18a802921875cdcce99958ee34
Kwon <kwonsh04011@gmail.com>
2026-02-07 20:29:46 +0900

    리스트 추가 기능 수정

--- index.js
+++ index.js
@@ -1,11 +1,80 @@
 "use strict";
 
+const mainContent = document.querySelector(".app__content");
 const goToAddPage = document.querySelector(".app__main");
 const appContent = document.querySelector(".app__content-card");
 
-const wordForm = document.querySelector(".content__container__text-box");
+const wordForm = document.querySelector(".content__container__form__text-box");
 const sentenceInput = document.querySelector("#sentence");
 const meanInput = document.querySelector("#mean");
+
+const cardTemplate = `
+<div class="app__content-card">
+            <div class="app__content__card__inner">
+              <div class="content__container__header">
+                <span class="content__numbers">1</span>
+                <!-- start of right menu-->
+                <!-- <div class="content__input-box__menu">
+                  <button class="content__input-box__more-button">
+                    <svg viewBox="0 0 24 24" fill="currentColor">
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <button class="content__input-box_delete-button">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      width="20"
+                      height="20"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                      />
+                    </svg>
+                  </button>
+                </div> -->
+                <!-- End of right menu  -->
+              </div>
+              <form class="content__container__form__text-box" id="text-box">
+                <div class="content__container__text-box">
+                  <div class="content__sentence__inner">
+                    <input
+                      type="text"
+                      id="sentence"
+                      class="content__text-box__sentence"
+                    />
+                    <label for="sentence" class="label__sentence">문장</label>
+                  </div>
+                  <div class="content__mean__inner">
+                    <input
+                      type="text"
+                      id="mean"
+                      class="content__text-box__mean"
+                    />
+                    <label for="mean" class="label__mean">뜻</label>
+                  </div>
+                  <div class="content_text-box__submit_inner">
+                    <button
+                      class="content_text-box__submit"
+                      type="submit"
+                      form="text-box"
+                    >
+                      저장
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+  `;
 
 wordForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -17,16 +86,27 @@ function getInputValue() {
   const mean = meanInput.value.trim();
   if (!sentence || !mean) {
     alert("문장 또는 뜻을 입력해주세요");
+    return;
   }
   setWords(sentence, mean);
+  alert("저장되었습니다.");
+  wordForm.reset();
+  sentenceInput.focus();
 }
 
 function setWords(sentence, mean) {
-  localStorage.setItem(sentence, mean);
-  console.log(localStorage.getItem(sentence));
+  const id = Date.now();
+  const data = {
+    sentence,
+    mean,
+  };
+  localStorage.setItem(id, JSON.stringify(data));
 }
-// 뜻도 value 값을 이용해서 문장을 가져올 수 있는 방법은?
-// 저장 후 안내 메시지 추가
+setWordsList();
+
+function setWordsList() {
+  mainContent.insertAdjacentHTML("beforeend", cardTemplate);
+}
 
 goToAddPage.addEventListener("click", (event) => {
   const linkEl = event.target.closest("[data-link]");
