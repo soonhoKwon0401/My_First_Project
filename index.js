d04c2b182f7c2553d482e1526b58a006211f1a71
Kwon <kwonsh04011@gmail.com>
2026-02-08 23:38:27 +0900

    javascript 수정

--- index.js
+++ index.js
@@ -9,10 +9,43 @@ const wordForm = document.querySelector(".content__container__form__text-box");
 const sentenceInput = document.querySelector("#sentence");
 const meanInput = document.querySelector("#mean");
 
-const createWordCardHeard = `
+const createWordCardSideMenu = `
+<div class="content__input-box__menu">
+                  <button class="content__input-box__more-button">
+                    <svg viewBox="0 0 24 24" fill="currentColor">
+                      <path
+                        fill-rule="evenodd"
+                        d="M10.5 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0zm0 6a1.5 1.5 0 113 0 1.5 1.5 0 01-3 0z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                  <button class="content__input-box_delete-button">
+                    <svg
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      width="20"
+                      height="20"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
+                      />
+                    </svg>
+                  </button>
+                </div>
+`;
+
+const createWordCardHeard = (showMenu = false) => {
+  return `
   <div class="content__container__header">
     <span class="content__numbers"></span>
+    ${showMenu ? createWordCardSideMenu : ""}
   </div>`;
+};
 
 const renderWordCardHeard = (container, html) => {
   container.insertAdjacentHTML("afterbegin", html);
@@ -22,8 +55,7 @@ const setWordCardId = (id) => {
   document.querySelector(".content__numbers").textContent = id;
 };
 
-const WordCardHeardHtml = createWordCardHeard;
-renderWordCardHeard(appContentInner, WordCardHeardHtml);
+renderWordCardHeard(appContentInner, createWordCardHeard(true));
 setWordCardId("1");
 
 wordForm.addEventListener("submit", (event) => {
