57ed578c468f6f782bbeb04f9be70f135e346e5f
SoonHoKwon <kwonsh04011@gmail.com>
2026-03-16 00:21:56 +0900

    renderBody 개선

--- index.js
+++ index.js
@@ -66,13 +66,18 @@ const wordCards = () => {
   return `<div class="app__content__cards__inner"> ${cardsHTML} </div>`;
 };
 
+const routes = {
+  "/": () => renderBody.add(),
+  "/add": () => renderBody.edit({ ...initialState, mode: "edit" }),
+  "/library": () => renderBody.lists(),
+};
+
 const renderBody = {
   edit: (data) => wordCard(data),
   view: (data) => wordCard(data),
   add: () => createWordCardAdd(),
+  lists: () => wordCards(),
 };
-
-// Library 적용 필요
 
 const cardContainer = (element) => `
   <div class="app__content-card">  
@@ -215,9 +220,7 @@ appContent.addEventListener("click", (event) => {
   createHtml(viewHTML);
   const card = document.querySelector(".app__content-card");
   setInputValue(card, data);
-  // 기존에 있던 값들도 불러오게끔 수정필요
-  // 만약 2번째 카드가 변경되는거면 해당 위치에서 수정 가능하도록 수정 필요
-  // 기존에 입력된 값이 Input에 있어야함
+  // 수정시 해당 카드의 값이 아닌 다른 카드의 값들이 불러옴 -bug
 });
 
 // edit으로 변경될때 Form 태그가 안불러옴... 확인 필요함
@@ -244,12 +247,6 @@ const getWords = () => {
     id: key,
     ...JSON.parse(localStorage.getItem(key)),
   }));
-};
-
-const routes = {
-  "/": () => renderBody.add(),
-  "/add": () => renderBody.edit({ ...initialState, mode: "edit" }),
-  "/library": () => wordCards(),
 };
 
 function getCurrentPath() {
